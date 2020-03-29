1ca3adc0b050151fd4f19853e6c49775ce335953
BlackPelican <BlackPelican@github.com>
2020-03-29 19:59:37 +0200

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# minimal vue template
+
+```bash
+npx degit blackpelican/templateVue project-name
+or
+git clone github.com/BlackPelican/templateVue
+```
+
+```bash
+npm install npm 
+run serve
+```
+
+[localhost:8080](localhost:8080)
+
+```bash
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">Hello</div>
+</template>
+
+<script>
+export default {
+  name: "App"
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap");
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html,
+body {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  font-family: "Open Sans";
+}
+</style>
